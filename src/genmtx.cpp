cf7bf1b9bfab767ad0f346cc544617f119acdea0
Tiago Onofre <taraujonofre@gmail.com>
2021-11-18 22:01:42 -0300

    Just some notes

--- src/genmtx.cpp
+++ src/genmtx.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+// function to generate and a function to write in a file
+
 int main(int argc, char * argv[]){
+
+    int n1, m1, n2, m2;
 
     std::cout << "Hello, SO, i'll defeat you this time motherfucker!\n";
 
